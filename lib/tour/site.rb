dece9624836b4af3e857efcc1ccc7fd6df1613f6
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-18 22:56:29 +0000

    site.rb

--- lib/tour/site.rb
+++ lib/tour/site.rb
@@ -4,5 +4,18 @@ class popular_sites
     <<-DOC.gsub/^\s*/,''
     DOC
     site_1 = self.new
-  end
+    site_1.name = "cape coast castle"
+    site_1.availability = true
+    site_1 url = "https://www.easytrackghana.com/tour-ghana_day-trips-accra.ph"
+
+    site_2 = self.new
+    site_2.name = "shai hill"
+    site_2.availability = true
+    site_2 url = "https://www.easytrackghana.com/tour-ghana_day-trips-accra.ph"
+
+    site_3 = self.new
+    site_3.name = "Boti falls"
+    site_3.availability = true
+    site_3 url = "https://www.easytrackghana.com/tour-ghana_day-trips-accra.ph"
+      end
 end

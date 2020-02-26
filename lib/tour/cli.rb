8204097fa26e134c4a83f1569800c18a26fa7b63
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-26 20:40:09 +0000

    bibini_cli

--- lib/tour/cli.rb
+++ lib/tour/cli.rb
@@ -16,9 +16,11 @@ class Cli
     puts " Popular sites Information retrieving ......... ".red
 
     Popular_sites
+    sites
     select_sites
     goodbye
   end
+
 
   def Popular_sites
     @sites = Popular_sites.sites
@@ -27,16 +29,24 @@ class Cli
   end
   end
 
+ def sites
+   @sites = select_sites.sites
+ end
+
   def select_sites
     sites = []
     input = nil
     while input != "exit"
-      puts "Enter the sites you want to visit or type exit"
+      puts "Here is a list of popular sites..."
+      puts " 1. Cape Coast Castle"
+      puts " 2. Shai Hills"
+      puts " 3. Boti falls"
+      puts "Enter 1,2 or 3 for any of the popular sites listed above or type exit".red
       input = gets.strip
       if input.to_i > 0
           puts  "#{sites}. {sites.name} - {sites.availability}"
         puts ""
-     puts "///////////////////////////////////////////////////////////////////////////"
+     puts "///////////////////////////////////////////////////////////////////////////".red
      puts ""
     # puts "#{concert_choice.name}".bold
         sites = Sites.find_by_index(input.to_i - 1)

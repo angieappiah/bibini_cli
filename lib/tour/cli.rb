796cdb576b86605db05c0a60f91e155308e810e5
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-28 18:04:55 +0000

    bibini_cli

--- lib/tour/cli.rb
+++ lib/tour/cli.rb
@@ -13,18 +13,13 @@ class Cli
     puts "   |     Welcome to Gh Tour!!    |".red.bold
     puts "   |||||||||||||||||||||||||||||||".red.bold
     puts ""
-    puts " popular sites Information retrieving ......... ".red
+    #puts " popular sites Information retrieving ......... ".red
 
-    sites
+    Scraper.scrape
     Popular_sites
-    select_sites
+    sites
     goodbye
   end
-
-def sites
-
-end
-
 
   def Popular_sites
     @sites = Popular_sites.scrape_sites
@@ -34,25 +29,24 @@ end
   end
 
 
-  def select_sites
-    sites = []
+  def sites
     input = nil
     while input != "exit"
+
+      input = gets.strip
+      if input.to_i > 0
+          puts  "#{Scraper.scrape}"
+        puts ""
+     puts "///////////////////////////////////////////////////////////////////////////".red
+     puts ""
+
+      elsif input == "p"
+      puts "PPOPULAR SITES".bold
       puts "Here is a list of some popular sites..."
       puts "Cape Coast Castle"
       puts "Shai Hills"
       puts "Boti falls"
       puts "Enter the popular sites you want to visit or type exit".red
-      input = gets.strip
-      if input.to_i > 0
-          puts  "#{sites}. {sites.name} - {sites.availability}"
-        puts ""
-     puts "///////////////////////////////////////////////////////////////////////////".red
-     puts ""
-        @sites.find_index("sites")
-        puts  "#{s}. {sites.name} - {sites.availability}"
-      elseif input == "popular_sites"
-       popular_sites
       else
         puts "No Selection entered"
       end
--- lib/tour/scraper.rb
+++ lib/tour/scraper.rb
@@ -1,10 +1,10 @@
 class Scraper
   def self.scrape
     doc = Nokogiri::HTML(open("https://www.getyourguide.com/accra-l506/half-day-old-and-new-accra-urban-experience-walking-tour-t15359"))
-    cards = doc.css(".activity-card")
-    cards.each do |card|
-    name = card.css(".activity-card-title")
-    url = card.css(".activity-card-link").href
+    scrape = doc.css(".activity-card")
+    scrape.each do |card|
+    name = scrape.css(".activity-card-title")
+    url = scrape.css(".activity-card-link")
     end
   end
 end

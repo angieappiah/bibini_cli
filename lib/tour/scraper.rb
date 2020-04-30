ee4989a983a710973baf83a553dd0a2d1b221410
Angela Appiah <alegnalawn2@yahoo.com>
2020-04-30 19:24:07 +0000

    bibini_cli

--- lib/tour/scraper.rb
+++ lib/tour/scraper.rb
@@ -1,13 +1,17 @@
 class SiteScraper
-element = []
+
   def self.scrape_site
    url = "https://touringghana.com/top-10-tourist-attractions/"
    html_to_elements = open(url)
    parsed_html_elements = Nokogiri::HTML(html_to_elements)
    site_elements = parsed_html_elements.css
    site_elements.css('h3').class('a').each do |element|
-     site_name = element.css('.site_list').text
    end
+  end
+
+  def self.scape_site_blurb(url)
+    html_to_elements = open(url)
+    parsed_html_elements = Nokogiri::HTML(html_to_elements)
   end
 
 end

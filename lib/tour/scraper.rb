a7ddc4e9d5e55ecff3c1cce1643cea12f5b1df3a
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-03 02:57:16 +0000

    bibini_cli

--- lib/tour/scraper.rb
+++ lib/tour/scraper.rb
@@ -1,10 +1,10 @@
-
+require 'pry'
 class Scraper
 
-  attr_reader :name, :location, :url
+  attr_reader :site_name, :location, :url
 
-  def initialize (name, location)
-    @name = name.capitalize
+  def initialize (site_name, location)
+    @site_name = site_name.capitalize
     @location = location.capitalize
     @url = "https://touringghana.com/top-10-tourist-attractions/".sub(" ", "--")
 
@@ -15,24 +15,18 @@ class Scraper
     parsed_html_elements = Nokogiri::HTML(html_to_elements)
   end
 
-  def scrape
-   parsed_html_elements = parse_url(@url)
-   binding.pry
-  end
 
   def self.scrape_site
    url = "https://touringghana.com/top-10-tourist-attractions/"
    html_to_elements = open(url)
    parsed_html_elements = Nokogiri::HTML(html_to_elements)
    site_elements = parsed_html_elements.css
-   #site_elements.css('h3').class('a').each do |element|
-     #site_name = element.css('.site_list').text
-     doc = Nokogiri::HTML(open("https://touringghana.com/top-10-tourist-attractions/"))
-     #doc.css('h3').class('a').each do |element|
-     #end
+   #site_elements.css('h3').class('a')
+   #end
+
   end
 
-    def self.scape_site_blurb(url)
+    def self.scrape_site_blurb(url)
       html_to_elements = open(url)
       parsed_html_elements = Nokogiri::HTML(html_to_elements)
     end

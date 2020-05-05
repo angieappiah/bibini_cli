b6a51df6b3d0c4b9ca667edc688637c7f89a3710
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-05 14:32:01 +0000

    bibini_cli

--- lib/tour/scraper.rb
+++ lib/tour/scraper.rb
@@ -7,16 +7,18 @@ class Scraper
   def self.scrape_site(url)
    html_to_elements = open(url)
    parsed_html_elements = Nokogiri::HTML(html_to_elements)
-   site_elements = parsed_html_elements.css('h3')
+   parsed_html_elements.css('h3').each do |elements|
+     #binding.pry
    #this is an array of site elements, we need to iterate over it and make new Site instances
-   site_name = parsed_html_elements.css('h3')[0].children.text
-   location = parsed_html_elements.css('h3')[0].css('a').attr('href')
-   url = parsed_html_elements.css('h3')[0].css('a').attr('href').value
-   new_site = Site.new(site_name,location,url)
-   binding.pry
+   site_name = elements.children.text
+   location = elements.css('a').attr('href')
+   url = elements.css('a').attr('href').value
+   Site.new(site_name,location,url)
+   #binding.pry
    #use the information that you scraped to make new instances of the sites
    #Site.new(name, description_url)
    #The description will be the second scrape
+   end
   end
 
     #def self.scrape_site_blurb(url)

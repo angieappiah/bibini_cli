c23f89c7ad6c0ff81b67b27134d4a520cee05ffa
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-07 15:41:37 +0000

    bibini_cli

--- lib/tour/scraper.rb
+++ lib/tour/scraper.rb
@@ -1,5 +1,5 @@
 require_relative "./site.rb"
-#require 'pry'
+require 'pry'
 class Scraper
 
 
@@ -7,15 +7,16 @@ class Scraper
   def self.scrape_sites(url)
    html_to_elements = open(url)
    parsed_html_elements = Nokogiri::HTML(html_to_elements)
-   parsed_html_elements.css('h3').each.with_index(1) do |elements, index|
+   parsed_html_elements.css('.item-details').each do |elements, index|
    #this is an array of site elements, we need to iterate over it and make new Site instances
-   site_name = elements.children.text
+   site_name = elements.children[1].text
    url = elements.css('a').attr('href').text
-   description = elements.css('.post-content').text
+   description = elements.children[5].text
    Site.new(site_name,description,url)
    puts "#{index}, #{site_name}"
    puts "#{url}".yellow
    puts ""
+   puts description
    #use the information that you scraped to make new instances of the sites
    #Site.new(name, description_url)
    #The description will be the second scrape
@@ -25,9 +26,10 @@ class Scraper
   def self.scrape_descriptions
    html_to_elements = (open("https://touringghana.com/top-10-tourist-attractions/"))
    parsed_html_elements = Nokogiri::HTML(html_to_elements)
-   parsed_html_elements.css('h1').each do |elements|
+   parsed_html_elements.css('t.d-block-span4').each do |elements|
+     binding.pry
    site_name = elements.children.text
-   description = elements.css('p').attr('href')
+   description = elements.css('.td-excerpt')
    url = elements.css('a').attr('href').text
    #description = Scraper.new(description)
    end

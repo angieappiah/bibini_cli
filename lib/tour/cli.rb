3acb6092a0a3bdb0d6925e4c11c923cd7600241f
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-10 04:18:01 +0000

    bibini_cli

--- lib/tour/cli.rb
+++ lib/tour/cli.rb
@@ -17,9 +17,6 @@ class Cli
     puts "These are the most popular sites in Ghana ...........".blue
 
     "https://touringghana.com/top-10-tourist-attractions/"
-     #Scraper.scrape_site
-    #give your user the opporunity to input
-    #when they input p, run te scraper from the Scraper class
 
 
   TourSiteScraper.scrape_sites
@@ -31,7 +28,6 @@ class Cli
 
   def options
     puts "Please enter a number (for details) or type exit".blue
-    #TourSiteScraper.scrape_sites
     input = nil
     while input != "exit"
      input = gets.strip
@@ -46,25 +42,22 @@ class Cli
 
 
     def list_sites
-
-      Site.all.each_with_index do |site, index|
-        #binding.pry
-        puts "#{index}. #{site.name}"
-        puts "#{site.url}".red
-        puts ""
-      end
+     Site.all.each_with_index do |site, index|
+     puts "#{index}. #{site.name}"
+     puts "#{site.url}".red
+     puts ""
     end
+   end
 
   def more_info(input)
     puts "WELCOME TO YOUR POPULAR SITE".blue
     puts "///////////////////////////////////////////////////////////////////////////".red
-#binding.pry
-  puts Site.all[input.to_i-1].description
-     #elsif input == "p"
-     #puts " Press p for the list of all the popular sites ".blue
+    puts ""
+    puts Site.all[input.to_i-1].description
+    puts ""
      #Once you've made the first scrape and created the instances, you list out the instances here
      #by calling on the SIte class, iterating over each site, and displaying each site's name
-     puts "Please enter a different number (for more details) or type exit".blue
+    puts "Please enter a different number (for more details) or type exit".blue
   end
 
 
